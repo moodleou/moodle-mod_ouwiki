moodleou/moodle-mod_forumng

Export: OU wiki, OU blog, and ForumNG export fail with Unicode #3743

feature/export/portfoliolib.php
@@ -222,16 +222,26 @@ class forumng_all_portfolio_caller extends forumng_portfolio_caller_base {
             $selected = false;
         }
 
-        // build the html for the export
-        $allhtml = "<html><body id='forumng-email'>\n";
-        $allhtml .= '<hr size="1" noshade="noshade" />';
+        // Set up the start of the XHTML file.
+        $allhtml = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' .
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' .
+                html_writer::start_tag('html', array('xmlns' => 'http://www.w3.org/1999/xhtml'));
+        $allhtml .= html_writer::tag('head',
+                html_writer::empty_tag('meta',
+                    array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=utf-8')) .
+                html_writer::tag('title', get_string('export', 'forumngfeature_export')));
+        $allhtml .= html_writer::start_tag('body') . "\n";
         $poststext = '';
         $postshtml = '';
+
         // we need a discussion object
         $discussion = mod_forumng_discussion::get_from_id($this->discussionid, $this->cloneid);
         $discussion->build_selected_posts_email($selected, $poststext, $postshtml,
                 array(mod_forumng_post::OPTION_EXPORT => true));
-        $allhtml .= $postshtml . '</body></html>';
+        $allhtml .= $postshtml;
+
+        // Finish the file.
+        $allhtml .= html_writer::end_tag('body') . html_writer::end_tag('html');
 
         // Remove embedded img and attachment paths.
         $plugin = $this->get('exporter')->get('instance')->get('plugin');

moodleou/moodle-mod_ouwiki

portfoliolib.php
@@ -157,15 +157,15 @@ abstract class ouwiki_portfolio_caller_base extends portfolio_module_caller_base
                 'date' => userdate($pageversion->timecreated),
                 'userlink' => ouwiki_display_user($user, $course->id)));
         $output .= html_writer::tag('p', html_writer::tag('small',
-        html_writer::tag('i', $lastchange)));
+                html_writer::tag('i', $lastchange)));
 
         // Main text
-        $output .= $formattedtext;
+        $output .= html_writer::tag('div', $formattedtext);
 
         // Word count
         if ($ouwiki->enablewordcount) {
             $wordcount = get_string('numwords', 'ouwiki', $pageversion->wordcount);
-            $output .= html_writer::empty_tag('br');
+            $output .= html_writer::tag('div', html_writer::empty_tag('br'));
             $output .= html_writer::tag('p',
             html_writer::tag('small', $wordcount),
             array('class' => 'ouw_wordcount'));
@@ -299,7 +299,18 @@ class ouwiki_page_portfolio_caller extends ouwiki_portfolio_caller_base {
     public function prepare_package() {
         global $CFG;
 
-        $pagehtml = $this->prepare_page($this->pageversion);
+        $pagehtml = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' .
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' .
+                html_writer::start_tag('html', array('xmlns' => 'http://www.w3.org/1999/xhtml'));
+        $pagehtml .= html_writer::tag('head',
+                html_writer::empty_tag('meta',
+                    array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=utf-8')) .
+                html_writer::tag('title', get_string('export', 'forumngfeature_export')));
+        $pagehtml .= html_writer::start_tag('body') . "\n";
+
+        $pagehtml .= $this->prepare_page($this->pageversion);
+
+        $pagehtml .= html_writer::end_tag('body') . html_writer::end_tag('html');
 
         $content = $pagehtml;
         $name = $this->make_filename_safe($this->pageversion->title === '' ?
@@ -368,11 +379,21 @@ class ouwiki_all_portfolio_caller extends ouwiki_portfolio_caller_base {
     public function prepare_package() {
         global $CFG;
 
-        $pagehtml = html_writer::tag('h1', s($this->ouwiki->name));
+        $pagehtml = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' .
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' .
+                html_writer::start_tag('html', array('xmlns' => 'http://www.w3.org/1999/xhtml'));
+        $pagehtml .= html_writer::tag('head',
+                html_writer::empty_tag('meta',
+                    array('http-equiv' => 'Content-Type', 'content' => 'text/html; charset=utf-8')) .
+                html_writer::tag('title', get_string('export', 'forumngfeature_export')));
+        $pagehtml .= html_writer::start_tag('body') . "\n";
+        $pagehtml .= html_writer::tag('h1', s($this->ouwiki->name));
 
         foreach ($this->pageversions as $pageversion) {
             $pagehtml .= $this->prepare_page($pageversion);
         }
+
+        $pagehtml .= html_writer::end_tag('body') . html_writer::end_tag('html');
 
         $content = $pagehtml;
         $name = $this->make_filename_safe($this->ouwiki->name) . '.html';