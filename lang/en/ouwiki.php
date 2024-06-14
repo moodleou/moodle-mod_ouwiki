cc781e53f0adb4ca54296300c83902c859117e05
Æon.VanGardingen [They/Them] <ig658@open.ac.uk>
2024-06-14 10:56:46 +0100

    Altered Collapse/Expand All Annotations to reflect accessibility change #725657
    Updated expand/collapse all labels and updated behat tests check for them #725657

--- lang/en/ouwiki.php
+++ lang/en/ouwiki.php
@@ -522,8 +522,8 @@ they are very determined. They could edit a page and wait until the timeout is a
 saving changes then very quickly editing it again.
 </p>';
 $string['endannotation'] = 'End of annotation';
-$string['expandallannotations'] = 'Expand annotations';
-$string['collapseallannotations'] = 'Collapse annotations';
+$string['expandallannotations'] = 'Expand all annotations';
+$string['collapseallannotations'] = 'Collapse all annotations';
 $string['showannotationicons'] = 'Show annotations';
 $string['hideannotationicons'] = 'Hide annotations';
 $string['errorcoursesubwiki'] = 'Must be &lsquo;No groups&rsquo; unless sub-wikis option is &lsquo;One wiki per group&rsquo;';
